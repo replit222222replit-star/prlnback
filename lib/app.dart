d7714d3c7c5b6f9f88bf63dc29c3ad082b14f38e
replit222222replit-star <replit222222replit@gmail.com>
2026-04-13 04:01:59 +0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,7 @@ import 'package:neo_genesis/screens/home_screen.dart';
 import 'package:neo_genesis/screens/auth_screen.dart';
 import 'package:neo_genesis/services/app_state.dart';
 import 'package:neo_genesis/services/voice_service.dart';
+import 'package:neo_genesis/services/screen_vision_service.dart';
 
 class NeoGenesisApp extends StatelessWidget {
   const NeoGenesisApp({super.key});
@@ -16,9 +17,10 @@ class NeoGenesisApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => AppState()),
         ChangeNotifierProvider(
-          create: (_) => VoiceService(
-            groqApiKey: dotenv.env['GROQ_API_KEY'] ?? '',
-          ),
+          create: (_) => VoiceService(groqApiKey: dotenv.env['GROQ_API_KEY'] ?? ''),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ScreenVisionService(groqApiKey: dotenv.env['GROQ_API_KEY'] ?? ''),
         ),
       ],
       child: MaterialApp(
@@ -27,9 +29,7 @@ class NeoGenesisApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: const Color(0xFF070B10),
           canvasColor: const Color(0xFF09141E),
-          textTheme: ThemeData.dark().textTheme.apply(
-                fontFamily: 'RobotoMono',
-              ),
+          textTheme: ThemeData.dark().textTheme.apply(fontFamily: 'RobotoMono'),
         ),
         home: const _RootScreen(),
       ),
@@ -39,7 +39,6 @@ class NeoGenesisApp extends StatelessWidget {
 
 class _RootScreen extends StatefulWidget {
   const _RootScreen();
-
   @override
   State<_RootScreen> createState() => _RootScreenState();
 }
@@ -48,32 +47,24 @@ class _RootScreenState extends State<_RootScreen> {
   @override
   void initState() {
     super.initState();
-    _requestPermissions();
+    _init();
   }
 
-  Future<void> _requestPermissions() async {
+  Future<void> _init() async {
     await [Permission.microphone, Permission.speech].request();
-    if (mounted) {
-      final voice = context.read<VoiceService>();
-      await voice.initialize();
-    }
+    if (mounted) await context.read<VoiceService>().initialize();
   }
 
   @override
   Widget build(BuildContext context) {
-    final isAuthenticated = context.select<AppState, bool>(
-      (s) => s.isAuthenticated,
-    );
+    final isAuthenticated = context.select<AppState, bool>((s) => s.isAuthenticated);
+    final isConnected = context.select<AppState, bool>((s) => s.isConnected);
 
-    // Запускаем wake word когда авторизован
-    if (isAuthenticated) {
-      final voice = context.read<VoiceService>();
-      final connected = context.select<AppState, bool>((s) => s.isConnected);
-      if (connected) {
-        voice.startWakeWordDetection();
-      } else {
-        voice.stopWakeWordDetection();
-      }
+    if (isAuthenticated && isConnected) {
+      context.read<VoiceService>().startWakeWordDetection();
+    } else {
+      context.read<VoiceService>().stopWakeWordDetection();
+      context.read<ScreenVisionService>().stopVision();
     }
 
     return isAuthenticated ? const HomeScreen() : const AuthScreen();
